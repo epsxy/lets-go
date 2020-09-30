c4eff02d8cacfb0d0ba9a7c16e86330b11f29c3e
epsxy <epsxy@users.noreply.github.com>
2020-09-30 02:23:31 +0200

    feat: resolve other list_again puzzles

--- src/lists/lists_again.go
+++ src/lists/lists_again.go
@@ -1,6 +1,9 @@
 package lists
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+)
 
 //InsertElementAtPosition : Insert an element at a given position into a list.
 func InsertElementAtPosition(lst []string, e string, p int) ([]string, error) {
@@ -32,11 +35,55 @@ func IntegersRange(s int, e int) ([]int, error) {
 	return r, nil
 }
 
-//Problem 23
-//Extract a given number of randomly selected elements from a list.
+// RemoveKthElementInt : Remove Kth element from an int list
+func RemoveKthElementInt(lst []int, k int) ([]int, error) {
+	r := []int{}
+	if k <= 0 {
+		return []int{}, errors.New("k should be strictly greater than 0")
+	}
+	if k > len(lst) {
+		return []int{}, errors.New("k should be lower than list len")
+	}
+	r = append(r, lst[0:k-1]...)
+	r = append(r, lst[k:len(lst)]...)
+	return r, nil
+}
 
-//Problem 24
-//Lotto: Draw N different random numbers from the set 1..M.
+// ExtractRandoms : Extract a given number of randomly selected elements from a list.
+func ExtractRandoms(lst []int, n int) ([]int, error) {
+	r := []int{}
+	t := append([]int{}, lst...)
+	if len(lst) <= 0 {
+		return []int{}, errors.New("List should not be empty")
+	}
+	if n > len(lst) {
+		return []int{}, errors.New("Number should be below list length")
+	}
+	for index := 1; index <= n; {
+		i := rand.Intn(len(t))
+		r = append(r, t[i])
+		// RemoveKthElement is a method from a previous problem
+		t, _ = RemoveKthElementInt(t, i+1)
+		index++
+	}
+	return r, nil
+}
+
+//Lotto : Draw N different random numbers from the set 1..M.
+func Lotto(n int, m int) ([]int, error) {
+	if m <= 0 {
+		return []int{}, errors.New("m should be >= 1")
+	}
+	if n > m {
+		return []int{}, errors.New("n should be <= m")
+	}
+	s, _ := IntegersRange(1, m)
+	r, err := ExtractRandoms(s, n)
+	if err != nil {
+		return []int{}, errors.New("ExtractRandoms failed")
+	}
+	return r, nil
+}
 
 //Problem 25
 //Generate a random permutation of the elements of a list.
--- src/lists/lists_again_test.go
+++ src/lists/lists_again_test.go
@@ -74,3 +74,89 @@ func TestShouldGenerateIntegersRangeWithBoundsOrderError(t *testing.T) {
 
 	assert.NotNil(t, err, "Error should not be nil")
 }
+
+/*===================== RemoveKthElementInt Tests =====================*/
+func TestRemoveKthElementInt(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	removed, err := RemoveKthElementInt(sample, 3)
+	expected := []int{1, 2, 4, 5, 6, 7, 8}
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, expected, removed, "Should remove 3rd element")
+}
+
+func TestRemoveFirstElementInt(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	removed, err := RemoveKthElementInt(sample, 1)
+	expected := []int{2, 3, 4, 5, 6, 7, 8}
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, expected, removed, "Should remove first element")
+}
+
+func TestRemoveLastElementInt(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	removed, err := RemoveKthElementInt(sample, 8)
+	expected := []int{1, 2, 3, 4, 5, 6, 7}
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, expected, removed, "Should remove last element")
+}
+
+func TestRemoveLastElementIntShouldReturnErrorWithLowerBound(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	_, err := RemoveKthElementInt(sample, 0)
+
+	assert.NotNil(t, err, "Error should not be nil")
+}
+
+func TestRemoveLastElementIntShouldReturnErrorWithGreaterBound(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	_, err := RemoveKthElementInt(sample, 9)
+
+	assert.NotNil(t, err, "Error should not be nil")
+}
+
+/*===================== ExtractRandoms Tests =====================*/
+func TestShouldExtractRandoms(t *testing.T) {
+	sample := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	res, err := ExtractRandoms(sample, 4)
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, []int{2, 8, 10, 1}, res, "Should extract randoms")
+}
+
+/*===================== Lotto Tests =====================*/
+func TestLottoShouldWork(t *testing.T) {
+	res, err := Lotto(5, 10)
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, []int{2, 8, 3, 9, 7}, res, "Should do lotto")
+}
+
+func TestLottoShouldFailIfMBelowOne(t *testing.T) {
+	_, err := Lotto(0, 0)
+
+	assert.NotNil(t, err, "Error should not be nil")
+	assert.EqualError(t, err, "m should be >= 1")
+}
+
+func TestLottoShouldFailIfNGreaterThanM(t *testing.T) {
+	_, err := Lotto(11, 10)
+
+	assert.NotNil(t, err, "Error should not be nil")
+	assert.EqualError(t, err, "n should be <= m")
+}
+
+func TestLottoShouldFailIfExtractRandomsFails(t *testing.T) {
+	res, err := Lotto(1, 1)
+
+	assert.Nil(t, err, "Error should be nil")
+	assert.Equal(t, []int{1}, res, "Should do very small lotto")
+}
